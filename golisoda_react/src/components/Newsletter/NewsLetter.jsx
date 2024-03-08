07de508036e449921120b8aee138a3b2d2d2715c
Ajith <ajith.m@capecomsolutions.com>
2024-03-08 15:08:02 +0530

    newsletter changes

--- golisoda_react/src/components/Newsletter/NewsLetter.jsx
+++ golisoda_react/src/components/Newsletter/NewsLetter.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Row, Form, Col, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
+import newsImg from "assets/newsletter.png";
+
 import "./styles.scss"
 
 function NewsLetterComponent() {
@@ -11,24 +13,33 @@ function NewsLetterComponent() {
   return (
     <div className="bg-primary-bg">
       <div className="container py-5">
-        <h4 className='text-white pb-3 text-center'>Subscribe to our newsletter</h4>
-        <Form onSubmit={handleSubmit(newsLetterSubmit)}>
-          <div className='newLetterCtr'>
-            <div className="hstack gap-3">
-              <Form.Group as={Row} controlId="firstName" className='w-100'>
-                <Col>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter email to subscribe"
-                    {...register('email', { required: true })}
-                  />
-                </Col>
-              </Form.Group>
-              <Button className='submitBtnNewsLetter'>Submit</Button>
-            </div>
+        <Row className='align-items-center'>
+          <Col md={6}>
+            <img src={newsImg} style={{ width: "80%" }} alt="" />
+          </Col>
+          <Col md={6}>
+            <h4 className='text-white pb-3'>Subscribe to our newsletter</h4>
+            <p className='text-white pb-5'>Subscribe now for hand-picked deals and offers, straight to your inbox.</p>
+            <Form onSubmit={handleSubmit(newsLetterSubmit)}>
+              <div className='newLetterCtr'>
+                <div className="hstack gap-3">
+                  <Form.Group as={Row} controlId="firstName" className='w-100'>
+                    <Col>
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter email to subscribe"
+                        {...register('email', { required: true })}
+                      />
+                    </Col>
+                  </Form.Group>
+                  <Button className='submitBtnNewsLetter'>Submit</Button>
+                </div>
 
-          </div>
-        </Form>
+              </div>
+            </Form>
+          </Col>
+        </Row>
+
       </div>
     </div>
   )
