9f43e322467d9ec5ffe8f5a19f67931554de2c67
Ajith <ajith.m@capecomsolutions.com>
2024-03-18 11:44:12 +0530

    changes..

--- golisoda_react/src/components/ProductsLists/ProductDetails/ProductListDetails.jsx
+++ golisoda_react/src/components/ProductsLists/ProductDetails/ProductListDetails.jsx
@@ -59,7 +59,8 @@ const ProductListDetails = ({
             </div>
           </div>
           {fetching ? (
-            <ProductGridPreloader />
+            activeGroup === "list" ? <ProductListPreloader /> :
+              <ProductGridPreloader />
           ) : (
             <>
               {products.length === 0 ? (
